b071f546b2c7e57ed497b80fd57a2c32902ca045
AlanaSR <svetlanka.shkola@gmail.com>
2025-04-28 18:00:41 +0500

    sql correct

--- src/main/java/ru/hogwarts/school/scripts421.sql
+++ src/main/java/ru/hogwarts/school/scripts421.sql
@@ -1,12 +1,12 @@
 alter table Student
 add constraint age_constraint check (age>16),
-add constraint age_default set default 20;
+alter column age set default 20;
 
 alter table Student
-add constraint name_unique unique (name) not null;
+add constraint name_unique unique (name);
 
 alter table Student
-alter column unique (name) not null;
+alter column name set not null;
 
 alter table Faculty
 add constraint faculty_unique unique (name, color);
